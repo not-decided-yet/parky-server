7cd96cbde621e2321aae6d74830aba769ff89eaf
Seosangwoo <sangwoo@scatterlab.co.kr>
2021-05-22 18:13:20 +0900

    feat: implement sign-up

---
+++ backend/parky/database/services.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Session
+
+from parky.database.models import User
+
+
+async def add_user(db: Session, name, ssn, user_id, password, public_key):
+    user = User(name=name, ssn=ssn, user_id=user_id, password=password, public_key=public_key)
+
+    db.add(user)
+    db.commit()
