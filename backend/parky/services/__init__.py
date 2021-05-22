90bb25857aa30cef9d09051e9849b95a8b304528
Seosangwoo <sangwoo@scatterlab.co.kr>
2021-05-23 04:24:07 +0900

    feat: implemet parking system

--- backend/parky/services/__init__.py
+++ backend/parky/services/__init__.py
@@ -1,6 +1,7 @@
+from .parking_lots import ParkingLotService
 from .user import UserService
 from .vehicle import VehicleService
 from .v2d import V2DRelayService
 from .firebase import FirebaseService
 
-__all__ = ["UserService", "VehicleService", "V2DRelayService", "FirebaseService"]
+__all__ = ["UserService", "VehicleService", "V2DRelayService", "FirebaseService", "ParkingLotService"]
---
+++ backend/parky/services/parking_lots.py
@@ -0,0 +1,121 @@
+import random
+from typing import Any, Dict, List, Optional
+
+from sqlalchemy.orm import Session
+
+from parky.database.models import ParkingLot
+from parky.utils import Singleton
+
+
+class ParkingLotService(Singleton):
+    def __init__(self):
+        self.vehicle_to_parking_place = dict()
+        self.reserved_user = set()
+
+    @staticmethod
+    def get_all_lots(db: Session) -> List[Dict[str, Any]]:
+        """
+        Get all lots in our database
+
+        :param db: SQLAlchemy DB Session
+        :returns: all lots in list
+        """
+        results = db.query(ParkingLot).all()
+        return [parking_lot.as_dict() for parking_lot in results]
+
+    def income_car(self, db: Session, parking_id: int, is_user: bool, vehicle_number: str) -> str:
+        """
+        handel incoming car
+
+        :param db: SQLAlchemy DB Session
+        :parking_id: Parking lot ID
+        :is_user Inform that this driver is our user
+        :vehicle_number Vehicle number of incoming car
+
+        :returns: Parking number for incoming car
+        """
+        result = db.query(ParkingLot).filter(ParkingLot._id == parking_id).first()
+        lot_dict = result.as_dict()
+
+        income_idx = -1
+
+        if is_user:
+            # choice maximum priority lots among rest
+            max_p = -1
+            for idx, (status, priority) in enumerate(zip(lot_dict["lots_status"], lot_dict["priority"])):
+                if max_p < priority and status != "X":
+                    max_p = priority
+                    income_idx = idx
+        else:
+            # choice random among rest
+            rest_lots_idx = [idx for idx, lot in enumerate(lot_dict["lots_status"]) if lot == "O"]
+            income_idx = random.choice(rest_lots_idx)
+
+        if lot_dict["lots_status"][income_idx] == "IR":
+            lot_dict["lots_status"][income_idx] = "O"
+
+        self.vehicle_to_parking_place[vehicle_number] = (parking_id, income_idx, lot_dict["lots_status"][income_idx])
+
+        lot_dict["lots_status"][income_idx] = "X"
+        result.lots_status = ",".join(lot_dict["lots_status"])
+        db.commit()
+
+        return lot_dict["lots_number"][income_idx]
+
+    def go_out_car(self, db: Session, vehicle_number: str, user_id: Optional[str] = None) -> int:
+        """
+        handel go_out car
+
+        :param db: SQLAlchemy DB Session
+        :parking_id: Parking lot ID
+        :vehicle_number Vehicle number of going out car
+        :user_id User id if driver is our user
+
+        :returns: 1 if success, 0 if fail
+        """
+        if vehicle_number not in self.vehicle_to_parking_place:
+            return 0
+
+        if user_id and user_id in self.reserved_user:
+            self.reserved_user.remove(user_id)
+
+        parking_id, income_idx, pre_state = self.vehicle_to_parking_place[vehicle_number]
+        result = db.query(ParkingLot).filter(ParkingLot._id == parking_id).first()
+
+        lot_dict = result.as_dict()
+        lot_dict["lots_status"][income_idx] = pre_state
+
+        result.lots_status = ",".join(lot_dict["lots_status"])
+        db.commit()
+
+        return 1
+
+    def reserve(self, db: Session, parking_id: int, user_id: str) -> int:
+        """
+        reserve lot
+
+        :param db: SQLAlchemy DB Session
+        :parking_id: Parking lot ID
+        :user_id User id if driver is our user
+
+        :returns: 1 if success, 0 if fail
+        """
+        if user_id in self.reserved_user:
+            return 0
+
+        result = db.query(ParkingLot).filter(ParkingLot._id == parking_id).first()
+        lot_dict = result.as_dict()
+
+        income_idx = -1
+        max_p = -1
+
+        for idx, (status, priority) in enumerate(zip(lot_dict["lots_status"], lot_dict["priority"])):
+            if max_p < priority and status == "O":
+                max_p = priority
+                income_idx = idx
+
+        lot_dict["lots_status"][income_idx] = "IR"
+        result.lots_status = ",".join(lot_dict["lots_status"])
+
+        self.reserved_user.add(user_id)
+        return 1
